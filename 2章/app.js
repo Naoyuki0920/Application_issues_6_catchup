9dff5b77ada4534117615639eb488307112fef8e
Naoyuki0920 <naoyuki-effort0920@docomo.ne.jp>
2021-09-03 09:05:58 +0900

    finish

---
+++ 2章/app.js
@@ -0,0 +1,16 @@
+let i;
+let num = 0;
+
+for(i = 1; i < 11; i++){
+  num = num + i;
+}
+
+alert('1から10まで足し算した結果は' + num + 'です');
+
+let a = 8;
+let b = 4;
+
+alert('8 + 4 =' + 12);
+alert('8 - 4 =' + 4);
+alert('8 × 4 =' + a * b);
+alert('8 ÷ 4 =' + a / b);
